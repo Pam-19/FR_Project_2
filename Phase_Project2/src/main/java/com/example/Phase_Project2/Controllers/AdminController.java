973110deac3792a09816f05e7aac1fd9f645517e
Pam-19 <112187640+Pam-19@users.noreply.github.com>
2023-07-12 17:41:45 +0530

    Added password change mechanism

--- Phase_Project2/src/main/java/com/example/Phase_Project2/Controllers/AdminController.java
+++ Phase_Project2/src/main/java/com/example/Phase_Project2/Controllers/AdminController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+	StringBuilder passkey= new StringBuilder("AdminPassword");
     private final ProductService productService;
     private final PurchaseHistoryService purchaseHistoryService;
     private final UserService userService;
@@ -56,6 +57,8 @@ public class AdminController {
     public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
         return productService.updateProduct(id, product);
     }
+    
+    
 
     @DeleteMapping("/products/{id}")
     public String deleteProduct(@PathVariable Long id) {
@@ -85,10 +88,19 @@ public class AdminController {
     }
 
     
+    @PutMapping("/password")
+    public String changePassword(@RequestParam String oldPassword, @RequestParam String newPassword) {
+    		if (passkey.toString().equals(oldPassword)) {
+            passkey= new StringBuilder(newPassword);
+            return "Password changed successfully.";
+        }
+        
+        return "Failed to change password";
+    }
     @PostMapping("/login")
 	public String login(@RequestParam String username, @RequestParam String password) {
 		if(username.equals("Admin")) {
-			if( password.equals("AdminPassword")) {
+			if( password.equals(passkey.toString())) {
 			return "Welcome Admin!!!";	
 		}
 		else { 
--- Phase_Project2/src/main/java/com/example/Phase_Project2/Controllers/UserController.java
+++ Phase_Project2/src/main/java/com/example/Phase_Project2/Controllers/UserController.java
@@ -1,14 +1,21 @@
 package com.example.Phase_Project2.Controllers;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Phase_Project2.Models.Product;
 import com.example.Phase_Project2.Models.PurchaseHistory;
 import com.example.Phase_Project2.Models.User;
+import com.example.Phase_Project2.Services.ProductService;
 import com.example.Phase_Project2.Services.PurchaseHistoryService;
 import com.example.Phase_Project2.Services.UserService;
 
@@ -18,12 +25,19 @@ public class UserController {
 
 	private final UserService userService;
     private final PurchaseHistoryService purchaseHistoryService;
+    private final ProductService productService;
 
 	@Autowired
-	public UserController(UserService userService,PurchaseHistoryService purchaseHistoryService) {
+	public UserController(UserService userService,PurchaseHistoryService purchaseHistoryService,ProductService productService) {
 		this.userService=userService;
 		this.purchaseHistoryService = purchaseHistoryService;
+		this.productService=productService;
 	}
+	
+	@GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
 	
 	@PostMapping
 	public User createUser(@RequestBody User user) {
@@ -40,6 +54,9 @@ public class UserController {
 	    // Save the PurchaseHistory object
 	    return purchaseHistoryService.savePurchaseHistory(purchaseHistory);
 	}
+	
+	
+
 	
 	@DeleteMapping
     public String deleteUser(@RequestParam String username, @RequestParam String password) {
